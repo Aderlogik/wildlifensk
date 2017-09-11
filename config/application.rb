f5a15668578aaa57a2a8c4a595f4f0602679e54a
Jaywant <jaydhomse@gmail.com>
2017-09-12 00:19:31 +0530

    chng

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module NasikWildlife
   class Application < Rails::Application
   
   config.assets.initialize_on_precompile = false
-  config.assets.precompile += ['styles.css']
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
